e36af0fb07e0bd312c319ca23a506b7871631f4a
mido1 <mido.15897@gmail.com>
2023-10-23 14:45:47 +0300

    react-level2-part2 2 - insertBook  createAsyncThunk - http://localhost:3005/books

--- src/store/bookSlice.js
+++ src/store/bookSlice.js
@@ -11,10 +11,30 @@ export const getBooks = createAsyncThunk('book/getBooks', async (_, thunkAPI) =>
     }
 });
 
+export const insertBook = createAsyncThunk(
+    'book/insertBook',
+    async (bookData, thunkAPI) => {
+        const {rejectWithValue} = thunkAPI;
+        try {
+            const res = await fetch('http://localhost:3005/books', {
+                method: 'POST',
+                body: JSON.stringify(bookData),
+                headers: {
+                    'Content-type' : 'application/json; charset=UTF-8'
+                }
+            });
+            const data = await res.json();
+            return data;
+        } catch (error) {
+            return rejectWithValue(error.message);
+        }
+    });
+
 const bookSlice = createSlice({
     name: 'book',
     initialState: {books: [], isLoading: false, error: null},
     extraReducers: {
+        // getBooks
         [getBooks.pending]: (state, action) => {
             state.isLoading = true;
             state.error = null;
@@ -24,6 +44,20 @@ const bookSlice = createSlice({
             state.books = action.payload;
         },
         [getBooks.rejected]: (state, action) => {
+            state.isLoading = false;
+            state.error = action.payload;
+        },
+
+        // insertBook
+        [insertBook.pending]: (state, action) => {
+            state.isLoading = true;
+            state.error = null;
+        },
+        [insertBook.fulfilled]: (state, action) => {
+            state.isLoading = false;
+            state.books.push(action.payload);
+        },
+        [insertBook.rejected]: (state, action) => {
             state.isLoading = false;
             state.error = action.payload;
         },
